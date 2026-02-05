dc67bfafd76fffd82e9ae8f67704c3783472ae85
denoc1 <176614645+denoc1@users.noreply.github.com>
2026-02-05 03:12:18 +0000

    template for project 2

---
+++ Project2.java
@@ -0,0 +1,15 @@
+//put your import statements here
+
+public class Project 2{
+
+
+    //put your static methods here (or after main if you prefer)
+
+
+    public static void main(String[] args){
+
+
+
+    }
+
+}
